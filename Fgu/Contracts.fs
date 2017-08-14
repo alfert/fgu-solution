beb1f2406cb66d36f0abbdf7ebe54b26cfe55951
Klaus Alfert <klaus.alfert@zuehlke.com>
2017-08-14 13:20:19 +0200

    First working increment

---
+++ Fgu/Contracts.fs
@@ -0,0 +1,24 @@
+﻿module Fgu.Contracts
+
+open Fgu.Events
+type Percent = float
+
+type QuotaShareContract = {
+    quota_share : Percent; // shared risk given to the reinsurances
+    quota_part : Percent; // the part of the quota_share for us
+    commission : float; 
+}
+
+// apply_events :: List Events -> QuotaShareContract -> float
+let apply_events_qs(events: Event list, contract: QuotaShareContract) : float = 
+    let factor = contract.quota_share * contract.quota_part
+    in
+        events 
+        |> List.sumBy(fun e -> e.loss * factor)
+
+type Contract = 
+    | QuotaShare of QuotaShareContract
+
+
+
+    
---
+++ Fgu/Events.fs
@@ -0,0 +1,17 @@
+﻿module Fgu.Events
+
+    open System
+
+    type Event = { 
+        day_of_year : int;
+        year : int;
+        loss : float }
+
+    let generate_events n the_year = 
+        let rnd = System.Random()
+        let single_event day = {day_of_year = day; year = the_year; loss = rnd.NextDouble()*(20.0*1000.0*1000.0) }
+        in
+            [1..n] 
+            |> List.map(fun x -> rnd.Next(365) + 1 )
+            |> List.map(fun d -> single_event(d) )
+            |> List.sortBy(fun ev -> ev.day_of_year)
