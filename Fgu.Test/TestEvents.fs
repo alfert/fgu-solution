b10a0c4cae4c078875e769d559ff46526c33d172
Klaus Alfert <klaus.alfert@zuehlke.com>
2017-08-14 15:20:41 +0200

    A simple test

---
+++ Fgu.Test/TestEvents.fs
@@ -0,0 +1,14 @@
+﻿module TestEvents
+    open NUnit.Framework
+    open FsUnit
+
+    open Fgu.Events
+
+    [<Test>]
+    let ``create an event`` () = 
+        1 |> should equal 1
+        let es = generate_events 1 2017
+        in
+            (List.length es) |> should equal 1
+        
+
