bc5925ec77d28c48a7b5603134268d145fec0d2f
Klaus Alfert <klaus.alfert@gmail.com>
2017-08-17 00:40:26 +0200

    Add an id to events and rename functions

--- Fgu/Events.fs
+++ Fgu/Events.fs
@@ -4,15 +4,18 @@
     open System
 
     type RiskEvent = { 
+        id : int;
         day_of_year : int;
         year : int;
         loss : float }
 
-    let generate_events (n : int) (the_year: int) : RiskEvent list= 
+    let generateEvents (n : int) (the_year: int) : RiskEvent list= 
         let rnd = System.Random()
-        let single_event day = {day_of_year = day; year = the_year; loss = rnd.NextDouble()*(20.0*1000.0*1000.0) }
+        let newDay() = rnd.Next(365) + 1
+        let singleEvent day anId = {day_of_year = day; year = the_year; 
+            loss = rnd.NextDouble()*(20.0*1000.0*1000.0); id = anId }
         in
-            [1..n] 
-            |> List.map(fun x -> rnd.Next(365) + 1 )
-            |> List.map(fun d -> single_event(d) )
+            [1..n]
+            |> List.map(fun x -> (newDay(), x))
+            |> List.map(fun (d, x) -> singleEvent d x  ) 
             |> List.sortBy(fun ev -> ev.day_of_year)
