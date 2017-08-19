e5815ae79ba04349e74ae418335d67459234c9ac
Klaus Alfert <klaus.alfert@gmail.com>
2017-08-19 12:47:14 +0200

    Generate events with risks and countries, selecting from a list of those. Together with a little test.

--- Fgu.Test/TestEvents.fs
+++ Fgu.Test/TestEvents.fs
@@ -9,13 +9,21 @@
     [<Test>]
     let ``create an event`` () = 
         1 |> should equal 1
-        let es = generateEvents 1 2017
+        let es = generateEvents 1 [DE] [Storm] 2017
         in
             (List.length es) |> should equal 1
         
 
     [<Property(QuietOnSuccess = true)>]
     let AddPlusOneProperty (eventCount) = 
+        let c = [DE]
+        let r = [Storm]
         eventCount >= 0 ==> 
-            let es = generateEvents eventCount 2017
+            let es = generateEvents eventCount c r 2017
             List.length(es) <= eventCount
+
+    [<Property(QuietOnSuccess = true)>]
+    let ``select an arbitrary element`` (xs : int list) = 
+        match selectOneOf xs with
+        | None   -> List.isEmpty xs 
+        | Some x -> List.contains x xs  
