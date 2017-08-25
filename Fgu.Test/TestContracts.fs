ff0bf5a9a326d61d1443498fa01516b64d052134
Klaus Alfert <klaus.alfert@gmail.com>
2017-08-25 11:41:13 +0200

    Some invariants about XL contracts

--- Fgu.Test/TestContracts.fs
+++ Fgu.Test/TestContracts.fs
@@ -51,3 +51,42 @@ module TestContracts
         let e = es.Head 
         let expectedPayment = min c.stop_loss (max 0.0 (e.loss - c.priority))
         l.loss |> should equal expectedPayment
+
+    [<Property()>]
+    let ``ìnvariant sums of XL contracts: ignore irrelevant risks``(eventCount : uint16) = 
+        let es = generateEvents (int eventCount) [DE] [Storm] 2017
+        let c = makeXLperRisk [Earthquake] [(xs 3000000.0  5000000.0); (xs 8000000.0  10000000.0) ]
+        es |> applyEventsXLperRisk c 
+        |> List.sumBy (fun e -> e.loss) 
+        = 0.0
+
+    [<Property(Verbose=true)>]
+    let ``ìnvariant sums of a single layered XL contract: ``(ls : uint32 list) = 
+        let es = generateEvents2  [DE] [Storm] 2017 (ls |> List.map float)
+        let minLoss = 3000000.0 
+        let maxLoss = 10000000.0
+        let c = makeXLperRisk [Storm] [(xs minLoss maxLoss) ]
+        let losses = es |> applyEventsXLperRisk c |> List.sumBy (fun e -> e.loss) 
+        // min(Haftung, max(0, X- Priorität))
+        let cut s = min (maxLoss - minLoss) (max 0.0 (s - minLoss))
+        in 
+            (es 
+            |> List.sumBy(fun e -> e.loss))
+            |> cut
+            .=. losses
+
+    [<Property(Verbose=true)>]
+    let ``ìnvariant sums of a doubled layered XL contract: ``(ls : uint16 list) = 
+        let es = generateEvents2  [DE] [Storm] 2017 (ls |> List.map float)
+        let minLoss =  3000.0 
+        let maxLoss = 10000.0
+        let c = makeXLperRisk [Storm] [(xs minLoss 5000.0); (xs 8000.0  maxLoss) ]
+        let losses = es |> applyEventsXLperRisk c |> List.sumBy (fun e -> e.loss) 
+        // min(Haftung, max(0, X- Priorität))
+        let cut s = min (maxLoss - minLoss) (max 0.0 (s - minLoss))
+        in 
+            (es 
+            // |> List.filter(fun e -> e.loss >= minLoss && e.loss <= maxLoss)
+            |> List.sumBy(fun e -> e.loss))
+            |> cut
+            .=. losses
